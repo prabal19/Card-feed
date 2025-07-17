454f889577db6c68d85f2bcc93e57833e2f4a4fc
prabal19 <prabaltrehan070@gmail.com>
2025-07-17 10:43:33 +0530

    icon resized

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -121,11 +121,11 @@ export function AppHeader({ popularCategoriesData = [] }: AppHeaderProps) {
             <>
               <Button variant="ghost" className="rounded-full relative" asChild>
                 <Link href="/notifications">
-                  <IoNotificationsOutline className="h-6 w-6" />
+                  <IoNotificationsOutline className="h-8 w-8" />
                     {unreadNotifications > 0 && (
-                        <span className="absolute top-1 right-1 flex h-2.5 w-2.5">
+                        <span className="absolute top-1 right-1 flex h-2 w-2">
                             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-                            <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-red-500"></span>
+                            <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
                         </span>
                     )}
                     <span className="sr-only">Notifications</span>
